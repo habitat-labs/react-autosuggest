68f5dc4e1b6012606783f4934d7b553bba353c48
Misha Moroshko <michael.moroshko@gmail.com>
2018-02-03 22:25:04 -0800

    Fix renderSuggestion query when input is blank

--- src/Autosuggest.js
+++ src/Autosuggest.js
@@ -419,7 +419,7 @@ export default class Autosuggest extends Component {
     const { value } = inputProps;
     const { valueBeforeUpDown } = this.state;
 
-    return (valueBeforeUpDown || value).trim();
+    return (valueBeforeUpDown === null ? value : valueBeforeUpDown).trim();
   }
 
   renderSuggestionsContainer = ({ containerProps, children }) => {
